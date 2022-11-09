f17fe1985d397a0a8d521e973e1c375f8465ef0c
Miau MIAU <117627076+lexi-miau@users.noreply.github.com>
2022-11-09 12:55:01 +0200

    Backtracking-unfinished

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+n=4
+m=4
+hospital = np.random.randint(2, size=(m+1, n+1))
+path = np.zeros((m+1, n+1), dtype=int)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+coord1=[1, 0, -1, 0]
+coord2=[0, 1, 0, -1]
+
+
+def valid(ii, jj):
+    print("abc")
+    if ii<0 or ii>m-1 or jj<0 or jj>n-1: return False
+    print("def")
+    return True
+
+def backtracking(i, j, pas):
+    for k in range(4):
+        ii=i+coord1[k]
+        jj=j+coord2[k]
+
+        if valid(ii, jj) is True:
+            path[ii][jj] = pas+1
+            if ii==0 or ii==m-2 or jj==0 or jj==n-2: show()
+            backtracking(ii, jj, pas+1)
+        path[ii][jj]=0
+
+
+def show():
+    for i in range(m):
+        for j in range(n):
+            print(path[i][j], end=' ')
+
+        print('\n')
+    print('\n')
+
+
+for i in range(m):
+    for j in range(n):
+        print(hospital[i][j], end=' ')
+    print('\n')
+
+print('\n')
+
+path[0][0]=1
+backtracking(2,1,1)
