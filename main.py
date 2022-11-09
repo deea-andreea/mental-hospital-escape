2c5d694eb456a31dc9b494b27a1fe0796fde2e5b
zinca andreea <87539435+deea-andreea@users.noreply.github.com>
2022-11-09 16:46:34 +0200

    Matrix GUI

--- main.py
+++ main.py
@@ -1,16 +1,75 @@
-# This is a sample Python script.
+import numpy as np
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
+import time
 
+hospital = np.random.randint(3, size=(20, 25))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+cnt = 0
+for i in range(1, 19):
+    a = random.randint(3, 4)
+    if a == 4 and cnt <=2 and hospital[i-1][0] != 4 and hospital[i-2][0] != 4 :
+        cnt = cnt + 1
+        hospital[i][0] = a
+    else:
+        hospital[i][0] = 3
 
+    a = random.randint(3, 4)
+    if a == 4 and cnt <= 2 and hospital[i-1][24] != 4 and hospital[i-2][24] != 4:
+        cnt = cnt + 1
+        hospital[i][24] = a
+    else:
+        hospital[i][24] = 3
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+cnt = 0
+for j in range(1, 23):
+    a = random.randint(3, 4)
+    if a == 4 and cnt <= 2 and hospital[19][j-1] != 4 and hospital[19][j-2] != 4 :
+        cnt = cnt + 1
+        hospital[19][j] = a
+    else:
+        hospital[19][j] = 3
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    a = random.randint(3, 4)
+    if a == 4 and cnt <= 2 and hospital[0][j-1] != 4 and hospital[0][j-2] != 4 :
+        cnt = cnt + 1
+        hospital[0][j] = a
+    else:
+        hospital[0][j] = 3
+
+for i in range(0, 20):
+    for j in range(0, 25):
+        print(hospital[i][j], end=" ")
+    print("\n")
+
+dpg.create_context()
+
+dpg.create_viewport(title="Escape")
+dpg.configure_viewport(0, x_pos=0, y_pos=0, width=1000, height=790)
+
+with dpg.window(label="Mental hospital", width=900, height=700):
+    with dpg.table(header_row=False) as table_id:
+
+        # use add_table_column to add columns to the table,
+        # table columns use child slot 0
+        for i in range(0, 26):
+            dpg.add_table_column()
+
+        for i in range(0, 20):
+            with dpg.table_row(height=30):
+                for j in range(0, 25):
+                    with dpg.table_cell():
+                        if hospital[i][j] == 0 or hospital[i][j] == 1:
+                            dpg.highlight_table_cell(table_id, i, j, [173, 216, 230])
+                        elif hospital[i][j] == 4:
+                            dpg.highlight_table_cell(table_id, i, j, [85, 187, 51])
+                            dpg.add_text("EXIT")
+                        else:
+                            dpg.highlight_table_cell(table_id, i, j, [66, 73, 82])
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
