c3a0b6e9941564a052525a3b56d2aa445418cf71
zinca andreea <87539435+deea-andreea@users.noreply.github.com>
2022-11-18 22:15:58 +0200

    Move player

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
+from operator import length_hint
 import sys
-
 import numpy as np
 import random
 import time
 import threading
-
 import dearpygui.dearpygui as dpg
 import dearpygui.demo as demo
 
 hospital = np.random.randint(3, size=(20, 25))
-#
-cnt = 0
 
+cnt = 0
 hospital[0][0] = 3
 hospital[19][0] = 3
 hospital[0][24] = 3
@@ -27,14 +25,12 @@ for i in range(1, 19):
         hospital[i][0] = a
     else:
         hospital[i][0] = 3
-
     a = random.randint(3, 4)
     if a == 4 and cnt <= 1 and hospital[i - 1][24] != 4 and hospital[i - 2][24] != 4 and hospital[i][23] != 2:
         cnt = cnt + 1
         hospital[i][24] = a
     else:
         hospital[i][24] = 3
-
 cnt = 0
 for j in range(1, 24):
     a = random.randint(3, 4)
@@ -43,14 +39,12 @@ for j in range(1, 24):
         hospital[19][j] = a
     else:
         hospital[19][j] = 3
-
     a = random.randint(3, 4)
     if a == 4 and cnt <= 1 and hospital[0][j - 1] != 4 and hospital[0][j - 2] != 4 and hospital[1][j] != 2:
         cnt = cnt + 1
         hospital[0][j] = a
     else:
         hospital[0][j] = 3
-
 for i in range(0, 20):
     for j in range(0, 25):
         print(hospital[i][j], end=" ")
@@ -64,8 +58,23 @@ dpg.create_viewport(title="Escape")
 
 dpg.configure_viewport(0, x_pos=0, y_pos=0, width=1000, height=790)
 
-with dpg.window(label="Mental hospital", width=900, height=700) as window1:
-    dpg.set_primary_window(window1, True)
+m = 20
+n = 25
+path = np.zeros((m + 1, n + 1), dtype=int)
+coord1 = [1, 0, -1, 0]
+coord2 = [0, 1, 0, -1]
+x = []
+y = []
+
+x1 = [0] * 150
+y1 = [0] * 150
+
+x2 = []
+y2 = []
+
+path2 = np.zeros((m + 1, n + 1), dtype=int)
+
+with dpg.window(label="Mental hospital", width=900, height=700):
     with dpg.table(header_row=False) as table_id:
 
         for i in range(0, 26):
@@ -78,33 +87,16 @@ with dpg.window(label="Mental hospital", width=900, height=700) as window1:
 
                         if hospital[i][j] == 0 or hospital[i][j] == 1:
                             dpg.highlight_table_cell(table_id, i, j, [173, 216, 230])
+                        elif hospital[i][j] == 10:
+                            dpg.highlight_table_cell(table_id, i, j, [200, 100, 45])
                         elif hospital[i][j] == 4:
                             dpg.highlight_table_cell(table_id, i, j, [85, 187, 51])
                             dpg.add_text("EXIT")
                         elif hospital[i][j] == 3 or hospital[i][j] == 2:
                             dpg.highlight_table_cell(table_id, i, j, [66, 73, 82])
 
-dpg.highlight_table_cell(table_id, 10, 12, [100, 20, 400])
-dpg.highlight_table_cell(table_id, 10, 11, [200, 0, 11])
-dpg.highlight_table_cell(table_id, 10, 13, [298, 0, 29])
-
-m = 20
-n = 25
-
-path = np.zeros((m + 1, n + 1), dtype=int)
-
-coord1 = [1, 0, -1, 0]
-coord2 = [0, 1, 0, -1]
-
-x = []
-y = []
-
-x1 = []
-y1 = []
-
-pasi = 0
-
-path2 = np.zeros((m + 1, n + 1), dtype=int)
+dpg.highlight_table_cell(table_id, 10, 12, [100, 21, 199])
+dpg.highlight_table_cell(table_id, 10, 11, [56, 48, 67])
 
 
 def show():
@@ -112,10 +104,6 @@ def show():
         for j in range(n):
             print(path[i][j], end=' ')
         print('\n')
-    print('\n')
-
-    for i in range(len(x)):
-        print(x[i], "     ", y[i])
     print('\n')
 
 
@@ -126,7 +114,7 @@ counter = 0
 def valid(ii, jj, steps):
     global minim
     if ii < 0 or ii > m - 1 or jj < 0 or jj > n - 1: return False
-    if hospital[ii][jj] == 2 or hospital[ii][jj] == 3 or hospital[ii][jj] == 5: return False
+    if hospital[ii][jj] == 2 or hospital[ii][jj] == 3: return False
     if steps > minim: return False
     if path[ii][jj] != 0: return False
     return True
@@ -144,24 +132,28 @@ def backtracking(i, j, pas, steps):
 
         steps = steps + 1
 
-        if valid(ii, jj, steps) is True:
+        if valid(ii, jj, steps) == 1:
+
             path[ii][jj] = pas + 1
             if hospital[ii][jj] == 4:
-
+                if counter == 0:
+                    counter = 1
+                    for i in range(2, len(x)):
+                        x2.append(x[i])
+                        y2.append(y[i])
 
                 if steps < minim:
                     minim = steps
 
-                    if counter==0:
-                        print(counter)
-                        print("chebujwncwcn")
-                        for i in range(len(x)):
-                            x1.append(x[i])
-                            y1.append(y[i])
-                            show()
+                    for i in range(len(x)):
+                        x1[i] = x[i]
+                        y1[i] = y[i]
 
-                    counter = 1
-                    #print(steps)
+                    for i in range(len(x), len(x1)):
+                        x1[i] = 25
+                        y1[i] = 20
+
+                    print(steps)
 
             backtracking(ii, jj, pas + 1, steps)
             path[ii][jj] = 0
@@ -171,38 +163,86 @@ def backtracking(i, j, pas, steps):
         steps = steps - 1
 
 
-for i in range(m):
-    for j in range(n):
-        print(hospital[i][j], end=' ')
-    print('\n')
-
 print('\n')
 path[10][11] = 1
-hospital[10][11] = 5
-
 for i in range(m):
     for j in range(n):
         print(path[i][j], end=' ')
     print('\n')
 cnt = 0
 backtracking(10, 11, 1, 0)
-print("abc")
+for i in range(len(x)):
+    print(x[i], end=' ')
+
+i = 10
+j = 12
 
 
-def move_prisoner():
-    dpg.highlight_table_cell(table_id, 10, 11, [0, 0, 0])
+def move_player(sender, app_data):
+    global i, j
+    if (app_data == 37 or app_data == 65) and hospital[i][j - 1] != 2 and hospital[i][j - 1] != 3:
+        # VEST
+        dpg.highlight_table_cell(table_id, i, j, [173, 216, 230])
+        j = j - 1
+        dpg.highlight_table_cell(table_id, i, j, [100, 21, 199])
+
+    if (app_data == 38 or app_data == 87) and hospital[i - 1][j] != 2 and hospital[i - 1][j] != 3:
+        # NORD
+        dpg.highlight_table_cell(table_id, i, j, [173, 216, 230])
+        i = i - 1
+        dpg.highlight_table_cell(table_id, i, j, [100, 21, 199])
+
+    if (app_data == 39 or app_data == 68) and hospital[i][j + 1] != 2 and hospital[i][j + 1] != 3:
+        # EST
+        dpg.highlight_table_cell(table_id, i, j, [173, 216, 230])
+        j = j + 1
+        dpg.highlight_table_cell(table_id, i, j, [100, 21, 199])
+
+    if (app_data == 40 or app_data == 83) and hospital[i+1][j] != 2 and hospital[i+1][j] != 3:
+        # SUD
+        dpg.highlight_table_cell(table_id, i, j, [173, 216, 230])
+        i = i + 1
+        dpg.highlight_table_cell(table_id, i, j, [100, 21, 199])
+
+    if hospital[i][j] == 4:
+        with dpg.window(label="Congrats!"):
+            dpg.add_text("You escaped!")
+
+
+def move_prisoner1():
+    # dpg.highlight_table_cell(table_id, 10, 11, [0, 0, 0])
+
     for i in range(len(x1)):
         dpg.highlight_table_cell(table_id, x1[i], y1[i], [200, 0, 11])
-        time.sleep(0.6)
+        time.sleep(2)
         dpg.highlight_table_cell(table_id, x1[i], y1[i], [173, 216, 230])
+
         print('\n')
     print('\n')
 
 
-move_thread = threading.Thread(name="move", target=move_prisoner, args=(), daemon=True)
-move_thread.start()
-#
+def move_prisoner2():
+    # dpg.highlight_table_cell(table_id, 10, 13, [0, 0, 0])
+
+    for i in range(len(x2)):
+        print(x2, "     ")
+        dpg.highlight_table_cell(table_id, x2[i], y2[i], [250, 0, 11])
+        time.sleep(0.7)
+        dpg.highlight_table_cell(table_id, x2[i], y2[i], [173, 216, 230])
+
+        print('\n')
+
+
+move_thread1 = threading.Thread(name="move", target=move_prisoner1, args=(), daemon=True)
+move_thread2 = threading.Thread(name="move", target=move_prisoner2, args=(), daemon=True)
+move_thread2.start()
+move_thread1.start()
+
+key_release_handler_parent = dpg.add_handler_registry()
+dpg.add_key_release_handler(callback=move_player, parent=key_release_handler_parent)
+
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
 dpg.destroy_context()
+
